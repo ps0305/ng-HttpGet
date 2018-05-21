82d1ae415c3413738a238d7026c96da83ce4c815
Pankaj Kumar Singh <34129569+ps0305@users.noreply.github.com>
2018-05-21 17:37:10 +0530

    components commits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { CountriesComponent } from './components/countries/countries.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CountriesComponent
   ],
   imports: [
     BrowserModule
--- src/app/services/countries.service.ts
+++ src/app/services/countries.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@angular/core';
+//importing Http class to connect to server
+import {Http,Response} from '@angular/http';
+//both map() and catch() function avaible in rxjs/Rx
+import "rxjs/Rx";
+import { Observable } from 'rxjs/Rx';
 
 @Injectable()
 export class CountriesService {
 
-  constructor() { }
+  constructor(private _http:Http) { }
 
+  public getCountries():any{
+    return this._http.get("https://restcountries.eu/rest/v2/all")
+    //.map for capturing positive response
+    .map((response:Response)=>{
+      return response.json();
+    });
+    .catch(this._handleError);
+  }
+  public _handleError(err){
+    console.log("Error raised...."+err);
+    //observable -to
+    return Observable.throw(err || "Internal server error")
+  }
 }
