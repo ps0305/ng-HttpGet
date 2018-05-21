0869a57b0d715d6920f4d721195e9c75ef397ae7
Pankaj Kumar Singh <34129569+ps0305@users.noreply.github.com>
2018-05-21 17:44:52 +0530

    component.html

--- src/app/components/countries/countries.component.ts
+++ src/app/components/countries/countries.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CountriesService } from '../../services/countries.service';
 
 @Component({
   selector: 'app-countries',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./countries.component.css']
 })
 export class CountriesComponent implements OnInit {
+  private data:any;
 
-  constructor() { }
+ constructor(private_service:CountriesService) { }
 
   ngOnInit() {
+    this._service.getCountries().subscribe(res=>
+    this.data=res,
+  err=>console.log(err));
   }
 
 }
